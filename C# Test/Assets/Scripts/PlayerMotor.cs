5790d96da40b3f74c17af2ddfd00b4d51a820226
StanleyYuGalan <yg_stan@yahoo.com>
2017-04-09 08:53:16 +0800

    Created New Prefabs and Optimized Game
    Created New Prefabs and Optimized Game

---
+++ C# Test/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMotor : MonoBehaviour
+{
+  private CharacterController controller;
+  private Vector3 moveVector;
+  private float speed = 15.0f;
+  private float verticalVelocity = 0.0f;
+  private float gravity = 12.0f;
+    private float jumpLimiter = 0f;
+    public Animator animator;
+    public GameObject youDied;
+    public GameObject youWin;
+    private Transform baseRotation;
+    private bool dead =false;
+  //Use this for installation
+  void Start ()
+  {
+      controller = GetComponent<CharacterController> ();
+        baseRotation = Vector3.zero;
+          }
+    //Update is called once per frame
+  void Update ()
+  {
+        if (!dead) {
+    moveVector = Vector3.zero;
+
+    if (controller.isGrounded)
+    {
+      verticalVelocity = -0.5f;
+    }
+    else
+    {
+      verticalVelocity -= gravity * Time.deltaTime;
+    }
+
+    moveVector.z = Input.GetAxisRaw("Horizontal") * speed*(-1);
+    baseRotation.eulerAngles = new Vector3(baseRotation.eulerAngles.x,baseRotation.eulerAngles.y, Input.GetAxisRaw("Horizontal") * -30);
+    moveVector.y = verticalVelocity;
+    moveVector.x = speed;
+            if (Input.GetAxisRaw("Jump") == 1 && jumpLimiter<1.2f)
+            {
+                moveVector = (jump(moveVector));
+                jumpLimiter += Time.deltaTime;
+            }
+           else
+            {
+                animator.SetBool("Jump", false);
+                if(controller.isGrounded)
+                    jumpLimiter = 0;
+            }
+    controller.Move (moveVector * Time.deltaTime);
+        }
+
+    }
+      void OnControllerColliderHit(ControllerColliderHit hit)
+   {
+        if (hit.gameObject.tag.Equals("Obstacle"))
+        {
+            killWinston();
+        }
+            
+        else if (hit.gameObject.tag.Equals("Victory"))
+        { winGame(); }
+            
+   }
+
+    void winGame()
+    {
+        youWin.SetActive(true);
+        dead = true;
+        Destroy(gameObject);
+        Debug.Log("Winston Won");
+    }
+    void killWinston()
+    {
+        animator.SetBool("Die", true);
+        youDied.SetActive(true);
+        dead = true;
+        Debug.Log("Winston Died");   
+         }
+    Vector3 jump(Vector3 moveVector)
+    {
+        animator.SetBool("Jump", true);
+        moveVector.y += 10.6f;
+        return moveVector;
+    }
+}
+
+
