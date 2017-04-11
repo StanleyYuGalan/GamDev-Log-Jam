d582e9ca4e1012358ba6cd6a9743d578ac3124e9
StanleyYuGalan <yg_stan@yahoo.com>
2017-04-11 08:36:10 +0800

    Added Main Menu & Upgraded Shaders
    Added Main Menu & Upgraded Shaders

---
+++ C# Test/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = 1;
+    }
+
+    public void loadMainMenu()
+    {
+     SceneManager.LoadScene("Main Menu");
+    }
+	
+    public void loadGame()
+    {
+    SceneManager.LoadScene("Endless Runner");
+    }
+
+    public void quitGame()
+    {
+        Debug.Log("Game quit");
+        Application.Quit();
+    }
+
+}
