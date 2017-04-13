88fd0dda5f614650a1f77588d6a17407dd7f0bc0
Calvin-Glein <glenn_matias@dlsu.edu.ph>
2017-04-14 05:00:02 +0800

    new

---
+++ C# Test/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour {
+
+
+    private Transform player;
+    private float level = -8f;
+    private RaycastHit hit;
+
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        Vector3 fwd = transform.TransformDirection(Vector3.down);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+
+
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+        {
+            print("Found an object - distance: " + hit.distance);
+
+           }
+        else
+        {
+            level = -10000;
+        }
+        transform.position = new Vector3(player.transform.position.x, level, player.transform.position.z);
+
+    }
+}
